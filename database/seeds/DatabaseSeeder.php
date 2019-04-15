296d5c3e4a3d561703b802542d4f5d898fc8b96d
ihsansaktia <ihsan.saktia@gmail.com>
2019-04-15 21:33:11 +0700

    Seed permissions before roles

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     {
     	$this->call([
         	UsersTableSeeder::class,
-            RolesTableSeeder::class,
             PermissionsTableSeeder::class,
+            RolesTableSeeder::class,
         ]);
     }
 }
