0e4bad768cd1fe37cb92beaa0a7eb33fbe00edff
Fadhriga Bestari <fadhrigabestari@gmail.com>
2019-04-23 21:59:20 +0700

    add update and delete project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -10,17 +10,22 @@ use Illuminate\Support\Facades\Validator;
 class ProjectController extends Controller
 {
   public function projectManagementHandler(Request $request) {
+    // \Log::info($request->all());
     $data = $request->all();
 
     $action = isset($data['action']) ? $data['action'] : null;
     if ($action == "create") {
       $this->createNewProject($data);
+    } else if ($action == "delete") {
+        $this->deleteProject($data);
+    } else if ($action == "update") {
+        $this->updateProject($data);
     } else if ($action == "fetchAll") {
       return $this->fetchAllProjects($data);
     }
   }
 
-  private function isRequestValid($data) {
+  private function isCreateRequestValid($data) {
     $validator = Validator::make($data, [
       'name' => ['required', 'string', 'max:255', 'unique:projects'],
       'description' => ['string', 'max:255'],
@@ -42,7 +47,7 @@ class ProjectController extends Controller
   }
 
   private function createNewProject($data) {
-    if (!($this->isRequestValid($data))) {
+    if (!($this->isCreateRequestValid($data))) {
       return null;
     }
 
@@ -51,6 +56,55 @@ class ProjectController extends Controller
     $newProject = Project::create($data);
 
     return $newProject;
+  }
+
+  private function deleteProject($data) {
+    $project = Project::find(
+        isset($data['id']) ? $data['id'] : null
+    );
+
+    if ($project) {
+        $project->delete();
+    }
+  }
+
+  private function isUpdateRequestValid($data) {
+    $validator = Validator::make($data, [
+      'id' => ['required', 'string'],
+      'name' => ['required', 'string', 'max:255', 'unique:projects'],
+      'description' => ['string', 'max:255'],
+      'file' => ['required', 'file', 'max:5000']
+    ]);
+
+    return !($validator->fails());
+  }
+
+  private function parseUpdateData($data) {
+    $uploadedFile = $data['file'];
+    $path = $uploadedFile->store('public/files');
+
+    return [
+      'id' => isset($data['id']) ? $data['id'] : null,
+      'name' => isset($data['name']) ? $data['name'] : null,
+      'description' => isset($data['description']) ? $data['description'] : null,
+      'file' => $path
+    ];
+  }
+
+  private function updateProject($data) {
+    if (!($this->isUpdateRequestValid($data))) {
+      return null;
+    }
+
+    $data = $this->parseUpdateData($data);
+
+    $project = Project::find($data['id']);
+    $project['name'] = $data['name'];
+    $project['description'] = $data['description'];
+    $project['file'] = $data['file'];
+    $project->save();
+
+    return $project;
   }
 
   private function fetchAllProjects() {
--- resources/js/components/Projects.vue
+++ resources/js/components/Projects.vue
@@ -24,19 +24,19 @@
                                 <tr>
                                     <th class="col-sm-1">ID</th>
                                     <th class="col-sm-2">Name</th>
-                                    <th class="col-sm-5">Description</th>
+                                    <th class="col-sm-4">Description</th>
                                     <th class="col-sm-2">File</th>
-                                    <th class="col-sm-2 text-right">Actions</th>
+                                    <th class="col-sm-3 text-right">Actions</th>
                                 </tr>
                                 <tr v-for="project in projects" v-bind:key="project.id">
                                     <th class="col-sm-1">{{ project.id }}</th>
                                     <th class="col-sm-2">{{ project.name }}</th>
-                                    <th class="col-sm-5">{{ project.description }}</th>
+                                    <th class="col-sm-4">{{ project.description }}</th>
                                     <th class="col-sm-2">{{ project.file }}</th>
-                                    <th class="row col-sm-2 text-right">
+                                    <th class="row col-sm-3 text-right">
                                         <button class="btn btn-warning" @click="openUpdate(project.id)"
-                                        data-toggle="modal" data-target="#modal-permission">Update</button>
-                                        <button class="btn btn-danger" @click="deletePermission(project.id)">Delete</button>
+                                        data-toggle="modal" data-target="#modal-update">Update</button>
+                                        <button class="btn btn-danger" @click="deleteProject(project.id)">Delete</button>
                                     </th>
                                 </tr>
                             </tbody>
@@ -104,6 +104,46 @@
             </div>
         </div>
         <!-- / create new modal -->
+
+        <!-- update modal -->
+        <div class="modal" id="modal-update" transition="modal">
+            <div class="modal-wrapper">
+                <div class="modal-dialog box box-default">
+                    <div class="box-header with-border">
+                        <slot name="header">Update project</slot>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span></button>
+                    </div>
+                    <form @submit.prevent="updateProject(project.id)" role="form">
+                        <div class="box-body">
+                            <div class="form-group">
+                                <label>Name
+                                    <input type="text" class="form-control"
+                                    placeholder="New name" v-model="project.name">
+                                </label>
+                            </div>
+                            <div class="form-group">
+                                <label>Description
+                                    <input type="text" class="form-control"
+                                    placeholder="New description" v-model="project.description">
+                                </label>
+                            </div>
+                            <div class="form-group">
+                                <label>File
+                                    <input type="file" @change="handleFile">
+                                </label>
+                        </div>
+                    </form>    
+                    <div class="modal-footer text-right">
+                        <slot name="footer">
+                            <button type="submit" class="btn btn-primary"
+                            @click="updateProject(project.id)" data-dismiss="modal">Update</button>
+                        </slot>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- / update modal -->
     </div> <!-- /.content-wrapper -->
 </template>
 
@@ -154,6 +194,13 @@ export default {
             
             this.pagination = pagination;
         },
+        handleFile: function(e) {
+            this.project.file = e.target.files || e.dataTransfer.files;
+            if (!this.project.file.length)
+                return
+            this.project.file = this.project.file[0];
+            console.log(this.project.file);
+        },
         createNewProject: function() {
             let url = 'http://localhost:8000/api/projects';
             let formData = new FormData();
@@ -178,12 +225,51 @@ export default {
                     this.fetchAllProjects();
                 })
         },
-        handleFile: function(e) {
-            this.project.file = e.target.files || e.dataTransfer.files;
-            if (!this.project.file.length)
-                return
-            this.project.file = this.project.file[0];
-            console.log(this.project.file);
+        deleteProject: function(id) {
+            let url = 'http://localhost:8000/api/projects';
+            let data = {
+                action: 'delete',
+                id: id
+            }
+            let options = {
+                method: 'post',
+                data,
+                url
+            }
+
+            axios(options)
+                .then(res => {
+                    console.log(res);
+                    this.fetchAllProjects();
+                })
+        },
+        openUpdate: function(id) {
+            this.project.id = id;
+        },
+        updateProject: function(id) {
+            let url = 'http://localhost:8000/api/projects';
+            let formData = new FormData();
+
+            formData.append('action', 'update');
+            formData.append('id', this.project.id);
+            formData.append('name', this.project.name);
+            formData.append('description', this.project.description);
+            formData.append('file', this.project.file);
+
+            let options = {
+                header: {
+                    'Content-Type': 'multipart/form-data'
+                },
+                method: 'post',
+                data: formData,
+                url
+            }
+
+            axios(options)
+                .then(res => {
+                    console.log(res);
+                    this.fetchAllProjects();
+                })
         }
     },
     created: function() {
